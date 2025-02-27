20a3ad82c4f79edf1c16e9c623eb0615033f2a26
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-02-27 13:31:37 +0100

    Made Room Class and the list

---
+++ TDV Projects/Text-room-game.py
@@ -0,0 +1,44 @@
+"""
+This project was made by Emanuel Baciu.
+Do not repost without permission.
+"""
+
+# Here go the imports.
+
+import arcade
+
+# End of imports
+
+# Classes go here.
+class Room:
+    def __init__(self):
+        description = ""
+        north = 0
+        east = 0
+        south = 0
+        west = 0
+# End of Classes.
+def main():
+    roomList = []
+    room1 = Room()
+    roomList.append(room1)
+    room2 = Room()
+    roomList.append(room2)
+    room3 = Room()
+    roomList.append(room3)
+    room4 = Room()
+    roomList.append(room4)
+    room5 = Room()
+    roomList.append(room5)
+    room6 = Room()
+    roomList.append(room6)
+# Function definition.
+
+# End of functions.
+
+# Variables go here.
+
+# End of variables.
+
+
+
