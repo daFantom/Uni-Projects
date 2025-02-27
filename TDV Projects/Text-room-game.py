7ed43b2001f2b5a1b5a975a03db27ebddb03f122
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-02-27 15:23:09 +0100

    Added default values to the Room class and changed all 0's to "None" in order to apply default value.

--- TDV Projects/Text-room-game.py
+++ TDV Projects/Text-room-game.py
@@ -11,7 +11,7 @@ import arcade
 
 # Classes go here.
 class Room:
-    def __init__(self, description:str,north:int,south:int,east:int,west:int):
+    def __init__(self, description:str="",north=0,south=0,east=0,west=0):
         self.description = description
         self.north = north
         self.south = south
@@ -22,19 +22,19 @@ class Room:
 # Function definition.
 def main():
     room_list = []
-    room1 = Room("You encounter yourself at the Hallway. You have 1 passage to the north, 2 to the east and 2 to the west.",1,0,2,2) # Hallway has 5 rooms it can enter.
+    room1 = Room("You encounter yourself at the Hallway. You have 1 passage to the north, 2 to the east and 2 to the west.",1,None,2,2) # Hallway has 5 rooms it can enter.
     room_list.append(room1)
-    room2 = Room("You're in the Bathroom, best to take a shower sooner than later! You have 2 entrances, south and east.",0,1,1,0) # Bathroom would be at the top left side of the map.
+    room2 = Room("You're in the Bathroom, best to take a shower sooner than later! You have 2 entrances, south and east.",None,1,1,None) # Bathroom would be at the top left side of the map.
     room_list.append(room2)
-    room3 = Room("Welcome to the Master Bedroom, best you don't do dirty things. You have 2 exits, north and east.",1,0,1,0) # Master bedroom would be below the Bathroom.
+    room3 = Room("Welcome to the Master Bedroom, best you don't do dirty things. You have 2 exits, north and east.",1,None,1,None) # Master bedroom would be below the Bathroom.
     room_list.append(room3)
-    room4 = Room("You happen to enter the Storage Room... However nothing is there, too bad! You have only one exit, east.",0,0,1,0) # Storage room only accessible from the Hallway.
+    room4 = Room("You happen to enter the Storage Room... However nothing is there, too bad! You have only one exit, east.",None,None,1,None) # Storage room only accessible from the Hallway.
     room_list.append(room4)
-    room5 = Room("Your chef instincts rush you to the Kitchen, what will you prepare today? You can exit from the south or west",0,1,0,1) # Kitchen will be on the top right side of the map.
+    room5 = Room("Your chef instincts rush you to the Kitchen, what will you prepare today? You can exit from the south or west",None,1,None,1) # Kitchen will be on the top right side of the map.
     room_list.append(room5)
-    room6 = Room("You enter the Dining room, there's nothing but a table. What are the devs on about? You have 2 exits, north and west.",1,0,0,1) # Dining Room will have access from Hallway and Kitchen.
+    room6 = Room("You enter the Dining room, there's nothing but a table. What are the devs on about? You have 2 exits, north and west.",1,None,None,1) # Dining Room will have access from Hallway and Kitchen.
     room_list.append(room6)
-    room7 = Room("The fresh air from the Balcony takes you back to your grandpa's house, good times eh? You can only exit from the south.",0,1,0,0) # Balcony only has access from the Hallway.
+    room7 = Room("The fresh air from the Balcony takes you back to your grandpa's house, good times eh? You can only exit from the south.",None,1,None,None) # Balcony only has access from the Hallway.
     room_list.append(room7)
     current_room = 0
     print(room_list[current_room].description)
