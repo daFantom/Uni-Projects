d4dafb3e4c6b2fb0947ada253b0b93689d91236e
Fantom <emanuel.baciu@edu.uah.es>
2025-10-11 16:18:23 +0200

    Update main.c for testing 2

--- Year2/OS/C Course from ft. Bro Code/Math Functions/main.c
+++ Year2/OS/C Course from ft. Bro Code/Math Functions/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <math.h>
 
-// Test Hello World
+// Bombo
 
 int main(){
     /* In order to do math functions, we must include the <math.h> header / library. Otherwise, we'd have an error.*/
@@ -50,3 +50,4 @@ int main(){
     return 0;
 
 }
+
